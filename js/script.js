2408db0b3d4e547ad095beb28d805b3af3266a00
sam_ <45014739+sbordel@users.noreply.github.com>
2024-05-17 12:11:42 -0400

    fixed page layout switch

--- js/script.js
+++ js/script.js
@@ -1,66 +1,37 @@
 $(document).ready(function(){
     const mql = window.matchMedia("(max-width: 1000px)", "(max-width: 1000px) and (max-height: 800px)");
-    mql.addEventListener("change", (event) => {
-      if (event.matches) {
-        window.location.href = "../index.html"; 
-        
-      //   $("section").hide();
 
-      //  $(".btn").on( "click", function() {
+    mqlHandler();
+    mql.addEventListener("change", mqlHandler);
 
-      //   let section = $("section");
-      //   var btnID = this.id;
-      //   btnID = btnID.replace("-btn", "");
 
-      //   console.log(btnID)
-
-      //   section.id
-
-      //   $(section).hide();
-      //   $("#" + btnID).show();
-
-      //  })
-
+    function mqlHandler(){
+      let content = $("#content");
+      let section = $("section");
+      if (mql.matches) {
+        console.log("working")
+                  content.attr("class", "content-scroll");
+                  section.attr("class", "section-mobile")
+                  $(section).css("display", "block");
       } else {
-        // $("section").show();
-        // console.log("desktop view");
+        content.attr("class", "content-fixed");
+        section.attr("class", "section-desktop")
+        $(section).css("display", "none");
+        $("#intro").css("display", "block");
       }
-    });
-    
+    }
+
+    $( ".menu" ).on( "click", function() {
+      let button = $(this).attr("id");
+        buttonID = button.replace("-btn", "");
+
+        let section = $(".section-desktop");
+      
+  
+        let sectionID = buttonID;
+        $(section).css("display", "none");
+        console.log($(section))
+        $("#" + sectionID).css("display", "block");
+    } );
 });
 
-// $(window).resize(function() {
-//     console.log($(window).width())
-//     if ($(window).width() <= 980 | ($(window).width() <= 980) & ($(window).height() <= 800))   {
-//         // $("#schedule").show();
-//         // $("#location").show();
-//         // $("#lodging").show();
-//         // $("#faq").show();
-//         // $("#gift").show();
-//         // $("#outreach").show();
-//         // $("#rsvp").show();
-//      }
-//      else {
-//         console.log('More than 960');
-//     //     /** desktop view **/
-//     //     $("#schedule").hide();
-//     //     $("#location").hide();
-//     //     $("#lodging").hide();
-//     //     $("#faq").hide();
-//     //     $("#gift").hide();
-//     //     $("#outreach").hide();
-//     //     $("#rsvp").hide();
-//     //  }
-
-//     });
-
-// $(document).ready(function(){
-
-
-//     // $("#hide").click(function(){
-//     //   $("p").hide();
-//     // });
-//     // $("#show").click(function(){
-//     //   $("p").show();
-//     // });
-//   });
