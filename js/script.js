0b372c2b63a3c15d0100d3adcf665b5bacd9e045
sam_ <45014739+sbordel@users.noreply.github.com>
2024-05-17 16:49:04 -0400

    added invitation and fixed resizing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    const mql = window.matchMedia("(max-width: 1000px)", "(max-width: 1000px) and (max-height: 800px)");
+    const mql = window.matchMedia("(max-width: 1100px)", "(max-width: 1100px) and (max-height: 700px)");
 
     mqlHandler();
     mql.addEventListener("change", mqlHandler);
@@ -9,28 +9,32 @@ $(document).ready(function(){
       let content = $("#content");
       let section = $("section");
       if (mql.matches) {
-        console.log("working")
                   content.attr("class", "content-scroll");
                   section.attr("class", "section-mobile")
                   $(section).css("display", "block");
+                  $(".letter").attr("src", "assets/letter@075x.png")
       } else {
         content.attr("class", "content-fixed");
         section.attr("class", "section-desktop")
         $(section).css("display", "none");
         $("#intro").css("display", "block");
+        $(".letter").attr("src", "assets/letter.png")
       }
     }
+
+    //make buttons turn dark 
+    // $("nav > a").on( "click", function() {
+    //   $(this > "img").css("opacity ")
+    // });
 
     $( ".menu" ).on( "click", function() {
       let button = $(this).attr("id");
         buttonID = button.replace("-btn", "");
 
         let section = $(".section-desktop");
-      
   
         let sectionID = buttonID;
         $(section).css("display", "none");
-        console.log($(section))
         $("#" + sectionID).css("display", "block");
     } );
 });
